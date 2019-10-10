e38b89f718277606201bdeb405e15b0071cc8c80
Sara Ahmed <sara.ahmed@irdeto.com>
2019-10-10 17:19:46 +0200

    First commit for spring boot

---
+++ src/main/java/sara/springFramework/springWepApp/SpringWepAppApplication.java
@@ -0,0 +1,13 @@
+package sara.springFramework.springWepApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWepAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWepAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sara/springFramework/springWepApp/SpringWepAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sara.springFramework.springWepApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWepAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
